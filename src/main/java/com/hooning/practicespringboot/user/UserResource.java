ab68a3272ff98d86608677af4d94ebe0824ce8dd
Hoon Cho <david.hoontime@gmail.com>
2020-10-03 21:15:03 +0200

    initial commit

---
+++ src/main/java/com/hooning/practicespringboot/user/UserResource.java
@@ -0,0 +1,44 @@
+package com.hooning.practicespringboot.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+    @Autowired
+    private UserDaoService userService;
+
+    // retrieveAllUsers
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userService.findAll();
+    }
+
+    // retrieveUser(int id)
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable final int id) {
+        User user = userService.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        userService.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
